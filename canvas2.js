67fedca499bf3ee77562f5faf6e03689ac7b69d6
= <=>
2015-03-10 14:48:22 +0000

    shapes

--- canvas2.js
+++ canvas2.js
@@ -1,10 +1,10 @@
-function triangle(ctx,x,y)
+function triangle(ctx,x,y,size,colour)
 {
-	ctx.fillStyle="#09F";
+	ctx.fillStyle=colour;
 	ctx.beginPath();
 	ctx.moveTo(x,y);
-	ctx.lineTo(x+50,y-100);
-	ctx.lineTo(x+100,y);
+	ctx.lineTo(x+size/2,y-size);
+	ctx.lineTo(x+size,y);
 	ctx.closePath();
 	ctx.fill();
 }
@@ -17,48 +17,33 @@ function circle(ctx,x,y,size,colour)
 	ctx.fill();
 }
 
-function init()
+function drawShapes(numShapes,canvasId)
 {
-	var colours = ["red", "yellow", "blue", "green"]
 
-	var canvas=document.getElementById("canvas");
+	var colours = ["red", "yellow", "blue", "green"]
+	var shapeFunctions = [triangle,circle]
+
+	var canvas=document.getElementById(canvasId);
 	if ( canvas.getContext )
 	{
 		var ctx=canvas.getContext("2d");
-		ctx.fillStyle="#F30";
-		ctx.fillRect(75,10,100,100);
 
-	 	circle(ctx,275,60,50);	 
-
-	 	for(i=0;i<40;i++)
+	 	for(i=0;i<numShapes;i++)
 	 	{
 	 		x = Math.floor(Math.random() * canvas.width); 	
 	 		y = Math.floor(Math.random() * canvas.height);
 	 		size = Math.floor(Math.random() * 50);
 	 		colour = colours[Math.floor(Math.random() * 4) ];
-	 		circle(ctx,x,y,size,colour);
-		}
-
-		triangle(ctx,375,110);		
-		triangle(ctx,0,210);
-
-	}
-
-	var canvas2=document.getElementById("canvas2");
-	if ( canvas2.getContext )
-	{
-		var ctx=canvas2.getContext("2d");
-	 		 
-	 	for(i=0;i<40;i++)
-	 	{
-	 		x = Math.floor(Math.random() * canvas2.width); 	
-	 		y = Math.floor(Math.random() * canvas2.height);
-	 		size = Math.floor(Math.random() * 50);
-	 		colour = colours[Math.floor(Math.random() * 4) ];
-	 		circle(ctx,x,y,size,colour);
+	 		shapeFunction = shapeFunctions[Math.floor(Math.random() * 2)]
+	 		shapeFunction(ctx,x,y,size,colour);
 		}
 
 	}
+}
 
+function init()
+{
+	drawShapes(10,"canvas");
+	drawShapes(50,"canvas2");
 }
 onload=init;
